1e24f91571926d01060b7d5873587b24bfe2e364
Itamar000Git <itamarbabai98@gmail.com>
2025-11-20 15:16:26 +0200

    g_commit2

---
+++ Assets/Scripts/Player/InputMover.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+/**
+ * This component moves its object when the player clicks the arrow keys.
+ */
+public class InputMover : MonoBehaviour
+{
+    [Tooltip("Speed of movement, in meters per second")]
+    [SerializeField] float speed = 10f;
+
+    [SerializeField]
+    InputAction move = new InputAction(type: InputActionType.Value, expectedControlType: nameof(Vector2));
+
+    void OnEnable()
+    {
+        move.Enable();
+    }
+
+    void OnDisable()
+    {
+        move.Disable();
+    }
+
+    void Update()
+    {
+        Vector2 moveDirection = move.ReadValue<Vector2>();
+        Vector3 movementVector = new Vector3(moveDirection.x, moveDirection.y, 0) * speed * Time.deltaTime;
+        transform.position += movementVector;
+    }
+}
---
+++ Assets/Scripts/Utils/Bourders.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Bourders : MonoBehaviour
+{
+    [SerializeField] public Transform FrontWall; //parmeters ofr bounderies
+    [SerializeField] public Transform BackWall;
+    [SerializeField] public Transform LeftWall; //parmeters ofr bounderies
+    [SerializeField] public Transform RightWall;
+    [SerializeField] private float minY;
+    [SerializeField] private float maxY;
+    [SerializeField] private float minX;
+    [SerializeField] private float maxX;
+    [SerializeField] float padding = 0.5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        maxY = FrontWall.position.y - padding; //update values
+        minY = BackWall.position.y + padding;
+        maxX = RightWall.position.x - padding; //update values
+        minX = LeftWall.position.x + padding;
+
+    }
+
+    // Update is called once per frame
+   void FixedUpdate()
+    {
+        Vector3 mov = transform.position; //get the actual position
+        mov.y = Mathf.Clamp(mov.y, minY, maxY); //lock the val
+        mov.x = Mathf.Clamp(mov.x, minX, maxX); //lock the val
+        transform.position = mov;//updating
+
+    }
+}
